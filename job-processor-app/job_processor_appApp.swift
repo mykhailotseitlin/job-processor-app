e7e46c04c0fb8d6f6b44e5c14c08ad222eaacd2f
Mykhailo Tseitlin <tseytlin21@gmail.com>
2023-07-13 08:59:12 +0300

    Initial commit

---
+++ job-processor-app/job_processor_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  job_processor_appApp.swift
+//  job-processor-app
+//
+//  Created by Mykhailo Tseitlin on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct job_processor_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
